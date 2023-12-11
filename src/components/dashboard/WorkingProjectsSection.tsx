3779654b5fb20deddd36636fdd0abe5f4c5fbdee
zhou fang <linkfang@outlook.com>
2023-12-11 17:51:22 -0500

    Added handler for when data is empty

--- src/components/dashboard/WorkingProjectsSection.tsx
+++ src/components/dashboard/WorkingProjectsSection.tsx
@@ -2,7 +2,7 @@ import { STYLES } from '@/constants/styles'
 import ProjectsProgressCard from './ProjectsProgressCard'
 import { trpc } from '@/utils/trpc'
 import dayjs from 'dayjs'
-import { Spin } from 'antd'
+import { Empty, Spin } from 'antd'
 
 const WorkingProjectsSection = () => {
   const { data: projectsData, isLoading } = trpc.findManyProject.useQuery(
@@ -11,30 +11,41 @@ const WorkingProjectsSection = () => {
       select: (projects) => projects.filter((project) => project.status === 'On Going'),
     }
   )
-  return (
-    <div>
-      <h2 css={STYLES.sectionTitle}>WORKING PROJECTS</h2>
-      {isLoading ? (
+
+  const renderWorkingProjectsCard = () => {
+    if (isLoading) {
+      return (
         <div css={{ height: 150, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
           <Spin />
         </div>
-      ) : (
-        <div css={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gap: 30 }}>
-          {projectsData?.map(({ customer, name, startDate, endDate }) => {
-            const duration = dayjs(endDate).diff(startDate)
-            const timeLeft = dayjs(endDate).diff()
+      )
+    }
 
-            return (
-              <ProjectsProgressCard
-                key={`${customer}-${name}`}
-                customer={customer}
-                projectTitle={name}
-                progress={Number(((timeLeft / duration) * 100).toFixed(0))}
-              />
-            )
-          })}
-        </div>
-      )}
+    if (projectsData?.length === 0) return <Empty css={{ marginTop: 30 }} />
+
+    return (
+      <div css={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gap: 30 }}>
+        {projectsData?.map(({ customer, name, startDate, endDate }) => {
+          const duration = dayjs(endDate).diff(startDate)
+          const timeLeft = dayjs(endDate).diff()
+
+          return (
+            <ProjectsProgressCard
+              key={`${customer}-${name}`}
+              customer={customer}
+              projectTitle={name}
+              progress={Number(((timeLeft / duration) * 100).toFixed(0))}
+            />
+          )
+        })}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2 css={STYLES.sectionTitle}>WORKING PROJECTS</h2>
+      {renderWorkingProjectsCard()}
     </div>
   )
 }
