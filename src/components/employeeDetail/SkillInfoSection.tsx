f43f5f6a02bfe417e2fd5a2cc96d52fcf9b4fa10
zhou fang <linkfang@outlook.com>
2024-01-08 22:05:15 -0500

    Removed expertise headers

--- src/components/employeeDetail/SkillInfoSection.tsx
+++ src/components/employeeDetail/SkillInfoSection.tsx
@@ -1,4 +1,4 @@
-import { COLORS, SIZES, STYLES } from '@/constants/styles'
+import { STYLES } from '@/constants/styles'
 import { renderSkillDots } from '@/utils/renderElement'
 import { trpc } from '@/utils/trpc'
 import { css } from '@emotion/react'
@@ -11,52 +11,32 @@ const SkillInfoSection = () => {
     { id: Number(query?.id as string) ?? 0 },
     {
       enabled: !!query?.id,
-      select: (data) => {
-        const formattedSkills: Record<string, { name: string; level: number }[]> = {}
-        data?.personSkills?.forEach((item) => {
-          let expertiseArr = formattedSkills[item.skill.field[0].name]
-          if (!expertiseArr) expertiseArr = []
-          expertiseArr.push({ name: item.skill.name, level: item.level })
-
-          formattedSkills[item.skill.field[0].name] = expertiseArr
-        })
-
-        return Object.entries(formattedSkills)
-      },
+      select: (data) => data?.personSkills,
     }
   )
 
   return (
     <div css={STYLES.cardCtn}>
       <h2 css={[STYLES.sectionTitle, css({ fontSize: 20 })]}>SKILLS</h2>
-
-      {isLoading ? (
-        <Spin />
-      ) : (
-        data?.map(([expertise, skills]) => (
-          <div key={expertise}>
-            <h3
-              css={{
-                fontSize: 16,
-                marginBottom: 20,
-                backgroundColor: COLORS.lightblue,
-                padding: '10px 15px',
-                borderRadius: SIZES.borderRadius,
-              }}
-            >
-              {expertise}
-            </h3>
-            <div css={{ display: 'flex', gap: 25, flexWrap: 'wrap', marginBottom: 30 }}>
-              {skills.map(({ name, level }) => (
-                <div key={name} css={STYLES.skillCard}>
-                  <p css={{ fontSize: 14, marginBottom: 8 }}>{name}</p>
-                  {renderSkillDots(level)}
-                </div>
-              ))}
+      <div
+        css={{
+          display: 'flex',
+          gap: 35,
+          flexWrap: 'wrap',
+          justifyContent: isLoading ? 'center' : 'flex-start',
+        }}
+      >
+        {isLoading ? (
+          <Spin css={{ marginTop: 30 }} />
+        ) : (
+          data?.map(({ skillId, level, skill: { name } }) => (
+            <div key={skillId} css={STYLES.skillCard}>
+              <p css={{ marginBottom: 8 }}>{name}</p>
+              {renderSkillDots(level)}
             </div>
-          </div>
-        ))
-      )}
+          ))
+        )}
+      </div>
     </div>
   )
 }
