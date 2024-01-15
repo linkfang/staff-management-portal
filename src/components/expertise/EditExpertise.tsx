1618edd921e96ad3305788b510c87fedea655189
zhou fang <linkfang@outlook.com>
2024-01-14 20:40:09 -0500

    Finished add expertise UI and basic logic

--- src/components/expertise/EditExpertise.tsx
+++ src/components/expertise/EditExpertise.tsx
@@ -1,5 +1,5 @@
 import { trpc } from '@/utils/trpc'
-import { Button, Form, Input, Spin } from 'antd'
+import { App, Button, Form, Input, Spin } from 'antd'
 import { useEffect, useState } from 'react'
 import { CloseOutlined } from '@ant-design/icons'
 import { COLORS, STYLES } from '@/constants/styles'
@@ -9,6 +9,7 @@ type TEditingData = { id: number; name: string }
 const EditExpertise = () => {
   const [editingData, setEditingData] = useState<TEditingData[]>()
 
+  const { notification } = App.useApp()
   const [form] = Form.useForm()
   const { data } = trpc.findManyExpertise.useQuery()
 
@@ -16,28 +17,39 @@ const EditExpertise = () => {
     if (data) setEditingData(data)
   }, [data])
 
-  if (!editingData) return <Spin />
+  if (!editingData || !data) return <Spin />
 
   return (
     <div>
       <div css={{ display: 'flex', gap: 20 }}>
-        <Form form={form}>
-          <Form.Item name="expertise">
+        <Form
+          form={form}
+          onFinish={() => {
+            const inputValue = form.getFieldValue('expertise')
+
+            if (!inputValue) return
+
+            if ([...data, ...editingData].find((item) => item.name.toLowerCase() === inputValue.toLowerCase())) {
+              notification.error({ message: `${inputValue} already exists` })
+              return
+            }
+
+            setEditingData((pre) => {
+              if (!pre) return [{ id: -1, name: inputValue }]
+              return [...pre, { id: -1, name: inputValue }]
+            })
+
+            form.setFieldValue('expertise', '')
+          }}
+        >
+          <Form.Item name="expertise" rules={[{ required: true }]}>
             <Input aria-label="expertise" placeholder="Expertise" />
           </Form.Item>
         </Form>
-        <Button
-          onClick={() => {
-            setEditingData((pre) => {
-              if (!pre) return [{ id: -1, name: form.getFieldValue('expertise') }]
-              return [...pre, { id: -1, name: 'Testing' }]
-            })
-          }}
-        >
-          Add
-        </Button>
+        <Button onClick={() => form.submit()}>Add</Button>
       </div>
-      <div css={{ marginBottom: 10, display: 'flex', gap: 20, flexWrap: 'wrap' }}>
+
+      <div css={{ marginTop: 10, marginBottom: 20, display: 'flex', gap: 20, flexWrap: 'wrap' }}>
         {data?.map((item) => {
           const isDeleted = !editingData.find((ele) => ele.id === item.id)
 
@@ -95,6 +107,48 @@ const EditExpertise = () => {
             </div>
           )
         })}
+
+        {editingData
+          .filter((item) => !data?.find((ele) => ele.id === item.id))
+          .map((item) => {
+            return (
+              <div
+                key={item.id}
+                css={[
+                  {
+                    transition: 'all 0.3s ease-out',
+                    position: 'relative',
+                    display: 'flex',
+                    backgroundColor: 'hsl(112, 50%, 90%)',
+                    alignItems: 'center',
+                    padding: '8px 16px',
+                    borderRadius: 6,
+                  },
+                ]}
+              >
+                <p>{item.name}</p>
+
+                <button
+                  css={[
+                    STYLES.buttonWithoutStyle,
+                    {
+                      marginLeft: 10,
+                      transition: 'all 0.3s ease-in',
+                      ':hover': { color: COLORS.errorRed },
+                    },
+                  ]}
+                  onClick={() => {
+                    setEditingData((pre) => {
+                      if (!pre) return pre
+                      return pre.filter((ele) => ele.name !== item.name)
+                    })
+                  }}
+                >
+                  <CloseOutlined css={{ transition: 'all 0.3s ease-out' }} />
+                </button>
+              </div>
+            )
+          })}
       </div>
     </div>
   )
